0ace47edb36d1e39883415bab9d20a81f1528fe5
flmarsou <marsouinflorent12@gmail.com>
2026-05-28 14:51:28 +0200

    Add ANSI codes for prettier debug

---
+++ include/ft_color.h
@@ -0,0 +1,11 @@
+#pragma once
+
+# define RESET   "\033[0m"
+# define BOLD    "\033[1m"
+
+# define RED     "\033[31m"
+# define GREEN   "\033[32m"
+# define YELLOW  "\033[33m"
+# define CYAN    "\033[36m"
+
+# define GRAY    "\033[90m"
