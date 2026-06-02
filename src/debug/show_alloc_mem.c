596e305a6e3cb04a316a4f24da9a231e325bda53
flmarsou <marsouinflorent12@gmail.com>
2026-06-02 11:48:31 +0200

    Add better totals (alloc, free, all, raw)

--- src/debug/show_alloc_mem.c
+++ src/debug/show_alloc_mem.c
@@ -3,7 +3,7 @@
 # include <stdio.h>
 # include "ft_color.h"
 
-static void show_page(const page_t *page, size_t *total, size_t *total_raw)
+static void show_page(const page_t *page, total_t *total)
 {
     size_t page_index = 0;
 
@@ -20,8 +20,12 @@ static void show_page(const page_t *page, size_t *total, size_t *total_raw)
             void *start = (void *)(char *)chunk + sizeof(chunk_t);
             void *end   = start + chunk->size;
 
-            *total += chunk->size;
-            *total_raw += chunk->size + sizeof(chunk_t);
+            if (!chunk->free)
+                (*total).alloc += chunk->size;
+            else
+                (*total).free += chunk->size;
+            (*total).all += chunk->size;
+            (*total).raw += chunk->size + sizeof(chunk_t);
 
             if (!chunk->free)
                 printf("      %sChunk [%03zu]%s -> %s%s%p%s - %s%s%p%s : %s%zu bytes%s\n", CYAN, chunk_index, RESET, GREEN, BOLD, start, RESET, GREEN, BOLD, end, RESET, YELLOW, chunk->size, RESET);
@@ -41,24 +45,30 @@ static void show_page(const page_t *page, size_t *total, size_t *total_raw)
     printf("\n");
 }
 
-void show_alloc_mem()
+void show_alloc_mem(void)
 {
     pthread_mutex_lock(&g_malloc_mutex);
 
-    size_t total = 0;
-    size_t total_raw = sizeof(page_t);
+    total_t total;
+
+    total.alloc = 0;
+    total.free  = 0;
+    total.all   = 0;
+    total.raw   = sizeof(page_t);
 
     printf("%sTiny Head%s -> %s%s%p%s\n", CYAN, RESET, GREEN, BOLD, (void *)g_allocator.tiny, RESET);
-    show_page(g_allocator.tiny, &total, &total_raw);
+    show_page(g_allocator.tiny, &total);
 
     printf("%sSmall Head%s -> %s%s%p%s\n", CYAN, RESET, GREEN, BOLD, (void *)g_allocator.small, RESET);
-    show_page(g_allocator.small, &total, &total_raw);
+    show_page(g_allocator.small, &total);
 
     printf("%sLarge Head%s -> %s%s%p%s\n", CYAN, RESET, GREEN, BOLD, (void *)g_allocator.large, RESET);
-    show_page(g_allocator.large, &total, &total_raw);
+    show_page(g_allocator.large, &total);
 
-    printf("%sTotal    %s -> %s%zu bytes%s\n", CYAN, RESET, GREEN, total, RESET);
-    printf("%sTotal Raw%s -> %s%zu bytes%s\n", CYAN, RESET, GREEN, total_raw, RESET);
+    printf("%sTotal Allocated %s -> %s%zu bytes%s\n", CYAN, RESET, GREEN, total.alloc, RESET);
+    printf("%sTotal Freed     %s -> %s%zu bytes%s\n", CYAN, RESET, GREEN, total.free, RESET);
+    printf("%sTotal All       %s -> %s%zu bytes%s\n", CYAN, RESET, GREEN, total.all, RESET);
+    printf("%sTotal Raw       %s -> %s%zu bytes%s\n", CYAN, RESET, GREEN, total.raw, RESET);
 
     pthread_mutex_unlock(&g_malloc_mutex);
 }
